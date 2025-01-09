479353735a3a7f6c26eb9a292184b7dc24a4c875
Babatunde <babatunde@wiseki.com>
2025-01-09 20:09:27 +0100

    new comit

--- components/Step.jsx
+++ components/Step.jsx
@@ -13,6 +13,7 @@ const Step = () => {
                 <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
                     <div className="flex relative pb-12">
                     <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                        
                         <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
                     </div>
                     <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
